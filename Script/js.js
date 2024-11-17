2ade54c4b406b80d03c20730886a97e592fbe445
Zackzep <103279212+Zackzep@users.noreply.github.com>
2024-11-16 20:34:15 -0800

    Updated to Get working
    Adjusted some code to get it functional again.  Still need to format process page and work on links

--- Script/js.js
+++ Script/js.js
@@ -1,67 +1,88 @@
-//const userInput = document.getElementById("userInput");
 const btnTest = document.getElementById("btnTest");
 const drinks = document.getElementsByClassName("drinks");
-//const pizzaSize = document.getElementsByClassName("pizzaSize");
-//const vegToppings = document.getElementsByClassName("vegToppings");
-//const meatToppings = document.getElementsByClassName("meatToppings");
-//const crust = document.getElementById("crust");
-//const displayselections = document.getElementById("displayselections");
-//const buyernotes = document.getElementById("buyernotes");
+const pizzaSize = document.getElementsByClassName("pizzaSize");
+const vegToppings = document.getElementsByClassName("vegToppings");
+const meatToppings = document.getElementsByClassName("meatToppings");
+const crust = document.getElementById("crust");
+const buyernotes = document.getElementById("buyernotes");
+const nameInput = document.getElementById("nameInput");  // Updated to match the new ID in HTML
+const streetAddressInput = document.getElementById("streetAddressInput"); // Updated ID
+const cityInput = document.getElementById("cityInput"); // Updated ID
+const stateInput = document.getElementById("stateInput"); // Updated ID
+const zipCodeInput = document.getElementById("zipCodeInput"); // Updated ID
+const phoneInput = document.getElementById("phoneInput"); // Updated ID
 const display = document.getElementById("display");
-
-
 let receipt = "";
-btnTest.addEventListener('click', function(){
-    //alert(userInput.value);
-    
-    for(let i = 0; i < drinks.length; i++){
-        if(drinks[i].checked){
-            receipt = drinks[i].value + "\n";
-            return;
+let total = 0;
+
+btnTest.addEventListener('click', function () {
+    receipt = "";  // Reset the receipt every time the button is clicked
+    total = 0;
+    // Get selected drink
+    for (let i = 0; i < drinks.length; i++) {
+        if (drinks[i].checked) {
+            receipt += 'Drink: ' + drinks[i].nextElementSibling.textContent + ' ($' + drinks[i].value + ')<br>';
+            total += parseFloat(drinks[i].value); // Add the drink price to total
+            break;
         }
-        
     }
-    
-    //for(let i = 0; i < pizzaSize.length; i++){
-        //if(pizzaSize[i].checked){
-            //alert(pizzaSize[i].value);
-            //return;
-        //}
-    //}
-    
-    //for(let i = 0; i < vegToppings.length; i++){
-        //if(vegToppings[i].checked){
-            //alert(vegToppings[i].value);
-        //}
-    //}
-    
-    //for(let i = 0; i < meatToppings.length; i++){
-        //if(meatToppings[i].checked){
-            //alert(meatToppings[i].value);
-        //}
-    //}
 
-    //alert(crust.value);
+    // Get selected pizza size
+    for (let i = 0; i < pizzaSize.length; i++) {
+        if (pizzaSize[i].checked) {
+            receipt += 'Pizza Size: ' + pizzaSize[i].nextElementSibling.textContent + ' ($' + pizzaSize[i].value + ')<br>';
+            total += parseFloat(pizzaSize[i].value); // Add the pizza size price to total
+            break;
+        }
+    }
 
-    //alert(buyernotes.value);
+    // Get selected meat toppings
+    let meatSelections = [];
+    for (let i = 0; i < meatToppings.length; i++) {
+        if (meatToppings[i].checked) {
+            meatSelections.push(meatToppings[i].nextElementSibling.textContent + ' ($' + meatToppings[i].value + ')');
+            total += parseFloat(meatToppings[i].value);
+        }
+    }
+    if (meatSelections.length > 0) {
+        receipt += 'Meat Toppings: ' + meatSelections.join(', ') + '<br>';
+    } else {
+        receipt += 'Meat Toppings: None<br>';
+    }
 
-    
+    // Get selected veggie toppings
+    let vegSelections = [];
+    for (let i = 0; i < vegToppings.length; i++) {
+        if (vegToppings[i].checked) {
+            vegSelections.push(vegToppings[i].nextElementSibling.textContent + ' ($' + vegToppings[i].value + ')');
+            total += parseFloat(vegToppings[i].value);
+        }
+    }
+    if (vegSelections.length > 0) {
+        receipt += 'Veggie Toppings: ' + vegSelections.join(', ') + '<br>';
+    } else {
+        receipt += 'Veggie Toppings: None<br>';
+    }
 
-    //displayselections.innerText = userInput.value;
-    //displayselections.innerHTML = drinks[6].value;
-    //displayselections.value = userInput.value;
-    
-    //displayselections.value = drinks.value;
-    
-    //displayselections.value = pizzaSize.value;
-    
-    //displayselections.value = vegToppings.value;
-    
-    //displayselections.value = meatToppings.value;
-    
-    //displayselections.value = crust.value;
-    
-    //displayselections.value = buyernotes.value;
-    
+    // Get selected crust
+    receipt += 'Crust Type: ' + crust.value + '<br>';
+
+    receipt += 'Total: $' + total.toFixed(2) + '<br>';
+
+    // Get special notes
+    receipt += 'Special Notes: ' + buyernotes.value + '<br>';
+
+    const name = nameInput.value;
+    const streetAddress = streetAddressInput.value;
+    const city = cityInput.value;
+    const state = stateInput.value;
+    const zipCode = zipCodeInput.value;
+    const phoneNumber = phoneInput.value;
+
+    receipt += 'Customer Name: ' + name + '<br>';
+    receipt += 'Address: ' + streetAddress + ', ' + city + ', ' + state + ' ' + zipCode + '<br>';
+    receipt += 'Phone Number: ' + phoneNumber + '<br>';
+
+    // Display the receipt in the PrintBox element
+    document.getElementById("PrintBox").innerHTML = receipt;
 });
-display.innerText = reciept;
